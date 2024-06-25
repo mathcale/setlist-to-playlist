4afbef76283c90844428544873de1e8e14ed45f4
Matheus Calegaro <hello@matheus.me>
2024-06-25 00:33:02 -0300

    feat(spotify/callback): return static html on auth callback success

--- internal/infra/web/handlers/spotify/callback_handler.go
+++ internal/infra/web/handlers/spotify/callback_handler.go
@@ -1,9 +1,12 @@
 package spotify
 
 import (
+	"embed"
+	"html/template"
 	"net/http"
 
 	client "github.com/mathcale/setlist-to-playlist/internal/clients/spotify"
+	"github.com/mathcale/setlist-to-playlist/internal/pkg/logger"
 	oauth2util "github.com/mathcale/setlist-to-playlist/internal/pkg/oauth2"
 	"github.com/mathcale/setlist-to-playlist/internal/pkg/responsehandler"
 	uc "github.com/mathcale/setlist-to-playlist/internal/usecases/spotify"
@@ -14,6 +17,7 @@ type SpotifyAuthCallbackWebHandlerInterface interface {
 }
 
 type SpotifyAuthCallbackWebHandler struct {
+	Logger               logger.LoggerInterface
 	CallbackUseCase      uc.SpotifyAuthCallbackUseCaseInterface
 	ResponseHandler      responsehandler.WebResponseHandlerInterface
 	GeneratedPKCECodes   oauth2util.GenerateOutput
@@ -21,7 +25,11 @@ type SpotifyAuthCallbackWebHandler struct {
 	SpotifyClientChannel chan client.AuthenticatedClient
 }
 
+//go:embed static/callback.html
+var callbackHTML embed.FS
+
 func NewSpotifyAuthCallbackWebHandler(
+	l logger.LoggerInterface,
 	uc uc.SpotifyAuthCallbackUseCaseInterface,
 	rh responsehandler.WebResponseHandlerInterface,
 	genCodes oauth2util.GenerateOutput,
@@ -29,6 +37,7 @@ func NewSpotifyAuthCallbackWebHandler(
 	ch chan client.AuthenticatedClient,
 ) SpotifyAuthCallbackWebHandlerInterface {
 	return &SpotifyAuthCallbackWebHandler{
+		Logger:               l,
 		CallbackUseCase:      uc,
 		ResponseHandler:      rh,
 		GeneratedPKCECodes:   genCodes,
@@ -44,13 +53,21 @@ func (h *SpotifyAuthCallbackWebHandler) Handle(w http.ResponseWriter, r *http.Re
 		return
 	}
 
-	h.ResponseHandler.Respond(w, http.StatusOK, map[string]string{
-		"message": "Spotify login completed, you can close this page now.",
-	})
+	h.renderCallbackPage(w)
 
 	authClient := client.AuthenticatedClient{
 		Client: *cl,
 	}
 
 	h.SpotifyClientChannel <- authClient
+}
+
+func (h *SpotifyAuthCallbackWebHandler) renderCallbackPage(w http.ResponseWriter) {
+	t, _ := template.ParseFS(callbackHTML, "static/callback.html")
+
+	w.Header().Add("Content-Type", "text/html")
+
+	if err := t.Execute(w, nil); err != nil {
+		h.Logger.Error("Error rendering callback page", err, nil)
+	}
 }
